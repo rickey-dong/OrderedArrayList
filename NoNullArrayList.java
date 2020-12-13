fd13ca21e2beaf2633e2d9fce2d585d1804e395d
Rickey Dong <rickeyd2004@gmail.com>
2020-12-13 14:08:17 -0500

    added the two constructors

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,6 +1,14 @@
 import java.util.ArrayList;
 public class NoNullArrayList<T> extends ArrayList<T>
 {
+  public NoNullArrayList()
+  {
+    super();
+  }
+  public NoNullArrayList(int initialCapacity)
+  {
+    super(initialCapacity);
+  }
   public T set(int index, T element)
   {
     if (element == null)
