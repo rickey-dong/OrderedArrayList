7bcd31333700ce50f3661652d16dff5a68ef40ea
Rickey Dong <rickeyd2004@gmail.com>
2020-12-13 16:00:45 -0500

    implemented public T set(index, element) for OrderedArrayList

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -23,6 +23,23 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
       return true;
     }
   }
+  public T set(int index, T element)
+  {
+    //[5,7,9]
+    //set(1,10)
+    //[5,9]
+    if (element == null)
+    {
+      throw new IllegalArgumentException("null cannot be added");
+    }
+    else
+    {
+      T thisWasRemoved = this.get(index);
+      this.remove(index);
+      this.add(this.whichIndex(element), element);
+      return thisWasRemoved;
+    }
+  }
   private int whichIndex(T element)
   {
     if (this.size() == 0)
