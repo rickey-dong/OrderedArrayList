54c976f6f8c526af886df40908793f55defb350c
Rickey Dong <rickeyd2004@gmail.com>
2020-12-13 15:52:25 -0500

    created add(index, element) method

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,5 +1,16 @@
 public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T>
 {
+  public void add(int index, T element)
+  {
+    if (element == null)
+    {
+      throw new IllegalArgumentException("null cannot be added");
+    }
+    else
+    {
+      super.add(this.whichIndex(element), element);
+    }
+  }
   private int whichIndex(T element)
   {
     if (this.size() == 0)
